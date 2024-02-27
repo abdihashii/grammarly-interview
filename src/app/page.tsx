7fda0ff481762562d11e13636d16c695679b6522
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-27 12:47:55 -0600

    Lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center p-24">
-			<section className="w-1/2 space-y-4 rounded-lg border border-black p-6 text-center">
+		<main className="flex min-h-screen flex-col items-center gap-8 p-24">
+			<section className="w-full space-y-4 rounded-lg border border-black p-6 text-center lg:w-1/2">
 				<h1 className="text-4xl font-medium">Grammarly Interview</h1>
 				<p className="text-sm text-slate-600">
 					TypeScript, Next14, shadcn/ui, Tailwindcss, lucide icons ...
